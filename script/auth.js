5970babe9e79beae5cc4a4cd0359372d7156155e
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2021-05-01 21:46:21 +0300

    Created auth.js

---
+++ script/auth.js
@@ -0,0 +1,46 @@
+import {baseUrl} from "./constants.js";
+
+export async function login(email, password){
+    const credentials = {
+        "email": email,
+        "password": password
+    };
+    const headers = new Headers({'Content-Type': 'application/json'});
+
+    const response = await fetch(url + "/login",
+      {method:'POST',
+      body: JSON.stringify(credentials),
+      headers: headers,
+    });
+
+    const message = await response.text();
+    if (response.ok)
+    {
+        localStorage.setItem("credentials", window.btoa(email + ":" + password));
+    }
+    console.log(message);
+    return response;
+}
+
+export async function createUser(email, password) {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    });
+    const data = {
+        "email": email,
+        "password": password
+    }
+    const response = await fetch(url + "/users",
+      {method:'POST',
+      body: JSON.stringify(data),
+      headers: headers,
+    })
+
+    const message = await response.json();
+    if (response.ok)
+    {
+        localStorage.setItem("credentials", window.btoa(email + ":" + password));
+    }
+    console.log(message);
+    return response;
+}
