b2ced5b0ee013f0dc79ea7fbe3634a9b9a8e45aa
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2021-05-20 04:27:08 +0300

    Created Footer component

--- online-pharmacy/src/App.js
+++ online-pharmacy/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from './components/Header.js'
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 export default class App extends React.Component {
     render() {
         return(
             <div>
                 <Header />
+
+                <Footer />
             </div>
         );
     }
---
+++ online-pharmacy/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../style.css";
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer class="border-top-blue bg-color-grey vertical-align-center">
+                <p>Online Pharmacy</p>
+            </footer>
+        );
+    }
+}
