c3b90481123d67d0a8379a586933b3bdf25e250b
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2021-05-23 15:50:49 +0300

    Created user-service

---
+++ online-pharmacy/src/scripts/user-service.js
@@ -0,0 +1,23 @@
+import configData from "../config.json";
+
+export function getCurrent() {
+    const headers = new Headers({'Authorization': `Basic ${localStorage.credentials}`});
+    return fetch(configData.BaseUrl + "/users/current", {
+        credentials: "same-origin",
+        headers: headers
+    })
+    .then(response => response.json())
+    .catch((err) => console.log(err));
+}
+
+export function editCurrent(data) {
+    const headers = new Headers({
+        'Authorization': `Basic ${localStorage.credentials}`,
+        'Content-Type': 'application/json'
+    });
+    return fetch(configData.BaseUrl + "/users/current",
+        {method:'PUT',
+        body: JSON.stringify(data),
+        headers: headers
+    });
+}
