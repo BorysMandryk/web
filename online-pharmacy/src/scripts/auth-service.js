655fbf9e607af341434dc92f5a9c1cfe1c8c31cc
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2021-05-23 23:50:59 +0300

    Created auth-service

---
+++ online-pharmacy/src/scripts/auth-service.js
@@ -0,0 +1,43 @@
+import configData from "../config.json";
+
+export async function login(email, password){
+    const credentials = {
+        "email": email,
+        "password": password
+    };
+    const headers = new Headers({'Content-Type': 'application/json'});
+
+    const response = await fetch(configData.BaseUrl + "/login",
+      {method:'POST',
+      body: JSON.stringify(credentials),
+      headers: headers
+    });
+
+    if (response.ok)
+    {
+        localStorage.setItem("credentials", window.btoa(email + ":" + password));
+    }
+    return response;
+}
+
+export async function createUser(email, password) {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    });
+    const data = {
+        "email": email,
+        "password": password
+    }
+    const response = await fetch(configData.BaseUrl + "/users",
+      {method:'POST',
+      body: JSON.stringify(data),
+      headers: headers
+    })
+
+    const message = await response.text();
+    if (response.ok)
+    {
+        localStorage.setItem("credentials", window.btoa(email + ":" + password));
+    }
+    return response;
+}
