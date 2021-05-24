dd83febaee694ac5b7c1d0a5795b9810ec8fa47f
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2021-05-24 05:17:44 +0300

    Set up routes

--- online-pharmacy/src/App.js
+++ online-pharmacy/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Catalog from './components/Catalog';
+import MedicationAbout from './components/MedicationAbout';
+import ShoppingBasket from './components/ShoppingBasket';
+import Order from './components/Order';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 export default class App extends React.Component {
     render() {
         return(
-            <div>
+            <BrowserRouter>
                 <Header />
-
+                <Switch>
+                    <Route exact path='/' component={Catalog} />
+                    <Route path='/medications/:medicationId' component={MedicationAbout} />
+                    <Route path='/shopping-basket' component={ShoppingBasket} />
+                    <Route path='/order' component={Order} />
+                    <Route path='/sign-in' component={SignIn} />
+                    <Route path='/sign-up' component={SignUp} />
+                    <Route path='/profile' component={Profile} />
+                </Switch>
                 <Footer />
-            </div>
+            </BrowserRouter>
         );
     }
 }
